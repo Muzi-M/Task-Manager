1b8b2ee3b2f7948754c0ec13432818e570a2f282
Muzi-M <diff.mlot@gmail.com>
2023-07-25 23:07:34 +0200

    handled all the routing and fuctionality of the application

--- server.js
+++ server.js
@@ -1,17 +1,156 @@
 const express = require("express");
-const port = 3000;
+const bodyParser = require("body-parser");
+
 const app = express();
+const PORT = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Middleware
 app.use(express.static("public"));
-app.set("views", "./views");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.get("/index", (req, res) => {
-  res.render(__dirname + "/views/index");
+// data for candidates and tasks
+let candidates = [];
+let currentId = 1;
+
+const tasks = [
+  { task: "Task 1", completed: false },
+  { task: "Task 2", completed: false },
+  { task: "Task 3", completed: false },
+  { task: "Task 4", completed: false },
+  { task: "Task 5", completed: false },
+];
+
+// Home route - show candidate list and weather
+app.get("/", (req, res) => {
+  const { filterCandidate, filterTask, filterCandidateId, sortBy } = req.query;
+
+  // Set the selectedTask based on the query parameter
+  const selectedTask = filterTask || "";
+
+  // Sort candidates based on the query parameter
+  let sortedCandidates = [...candidates];
+  if (sortBy === "id") {
+    sortedCandidates.sort((a, b) => a.id - b.id);
+  } else if (sortBy === "name") {
+    sortedCandidates.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  // Filter candidates based on the query parameters
+  const filteredCandidates = sortedCandidates.filter(
+    (candidate) =>
+      !filterCandidate ||
+      candidate.name.toLowerCase().includes(filterCandidate.toLowerCase())
+  );
+
+  // Function to filter candidates based on selected candidate ID
+  const filterCandidatesById = (candidates, selectedCandidateId) => {
+    if (!selectedCandidateId) return candidates;
+    return candidates.filter(
+      (candidate) => candidate.id === parseInt(selectedCandidateId)
+    );
+  };
+
+  const filteredCandidatesById = filterCandidatesById(
+    filteredCandidates,
+    filterCandidateId
+  );
+
+  // Function to filter candidates based on selected task
+  const filterCandidatesByTask = (candidates, selectedTask) => {
+    if (!selectedTask) return candidates;
+    return candidates.map((candidate) => {
+      const filteredCandidate = { ...candidate };
+      filteredCandidate.tasks = filteredCandidate.tasks.filter(
+        (task) => task.task === selectedTask
+      );
+      return filteredCandidate;
+    });
+  };
+
+  const filteredCandidatesByTask = filterCandidatesByTask(
+    filteredCandidatesById,
+    selectedTask
+  );
+
+  // Render the index page with candidate and task data
+  res.render("index", {
+    candidates: filteredCandidatesByTask,
+    tasks,
+    filterCandidate,
+    filterTask: selectedTask,
+    filterCandidateId,
+    sortBy,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+// Add new candidate
+app.post("/add", (req, res) => {
+  const { name } = req.body;
+  const id = currentId++; // Assign the currentId and then increment it
+
+  const candidateTasks = tasks.map((task) => ({ ...task })); // Create a copy of the tasks for the candidate
+  const candidate = { id, name, tasks: candidateTasks };
+  candidates.push(candidate);
+  res.redirect("/");
+});
+
+// Show edit form for a candidate
+app.get("/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const candidate = candidates.find(
+    (candidate) => candidate.id === parseInt(id)
+  );
+
+  if (candidate) {
+    res.render("edit", { candidate });
+  } else {
+    res.status(404).send("Candidate not found.");
+  }
+});
+
+// Update candidate's name
+app.post("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const candidate = candidates.find(
+    (candidate) => candidate.id === parseInt(id)
+  );
+  if (candidate) {
+    candidate.name = name;
+    res.redirect("/");
+  } else {
+    res.status(404).send("Candidate not found.");
+  }
+});
+
+// Delete a candidate
+app.get("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  candidates = candidates.filter((candidate) => candidate.id !== parseInt(id));
+  res.redirect("/");
+});
+
+// Mark a task as completed for a candidate
+app.get("/markCompleted/:candidateId/:taskId", (req, res) => {
+  const { candidateId, taskId } = req.params;
+
+  const candidate = candidates.find(
+    (candidate) => candidate.id === parseInt(candidateId)
+  );
+  if (candidate) {
+    const task = candidate.tasks.find((task) => task.task === taskId);
+    if (task) {
+      task.completed = true;
+    }
+  }
+
+  res.redirect("/");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
